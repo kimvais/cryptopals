94093b75dcf16f715547cd90a912836dbe2d2138
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-11-16 21:57:57 +0200

    Set 1, challenge 2

--- Tests.fs
+++ Tests.fs
@@ -5,7 +5,7 @@ open Xunit.Abstractions
 
 open Cryptopals.Utils
 
-type Tests(output:ITestOutputHelper) =
+type Set1(output:ITestOutputHelper) =
     let write result =
         output.WriteLine (sprintf "The result was %O" result)
         
@@ -14,4 +14,12 @@ type Tests(output:ITestOutputHelper) =
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
         let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
         let output = input |> readHex |> base64encode
+        Assert.Equal(output, expected)
+
+    [<Fact>]
+    let ``challenge 2``() =
+        let input = "1c0111001f010100061a024b53535009181c" |> readHex
+        let key = "686974207468652062756c6c277320657965" |> readHex
+        let expected = "746865206b696420646f6e277420706c6179"
+        let output = Seq.zip input key |> Seq.map ((fun (a, b) -> a ^^^ b) >> byteToHex) |> String.concat ""
         Assert.Equal(output, expected)
--- Utils.fs
+++ Utils.fs
@@ -1,8 +1,10 @@
 module Cryptopals.Utils
+
 open System
 
 
 let hexToByte (cs: char []) = Convert.ToByte(String(cs), 16)
+let byteToHex (b: byte) = b.ToString("x2")
 
 let readHex input =
     input
@@ -11,3 +13,6 @@ let readHex input =
     |> Seq.toArray
 
 let base64encode input = Convert.ToBase64String(input)
+
+let hexToString (bytes: byte []): string =
+    bytes |> Seq.map byteToHex |> String.concat ""
